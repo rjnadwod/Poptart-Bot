c089ce49e9960a512a18e02665de8f1a38a91bcd
rjnadwod <riley.nadwodny@gmail.com>
2021-09-30 15:00:01 -0500

    Create 'ping' command

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const { Client, Intents } = require('discord.js');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const deploy = require('./deploy-commands');
+require('dotenv').config();
 
 bot.on('ready', () => {
     console.log("Poptart Bot is online!");
-    bot.user.setActivity('Prefix is: . ', { type: "WATCHING"});
+    bot.user.setActivity('Use "/" for commands!', { type: "PLAYING"});
+    console.log("Activity set");
 })
+
+deploy.deployCommands();
 
 bot.on('error', console.error);
 
