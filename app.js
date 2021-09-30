c0a816ad92776a78ed9c635067d3a22612c311ec
rjnadwod <riley.nadwodny@gmail.com>
2021-09-30 12:44:49 -0500

    Use process env

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-require("dotenv").config();
-
 const { Client, Intents } = require('discord.js');
 const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+//const config = require('./config.json');
 
 bot.on('ready', () => {
     console.log("Poptart Bot is online!");
